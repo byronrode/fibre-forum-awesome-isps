12ae47b0f9d1db6cded7007e08b374a409033b2a
Byron Rode <byronrode@gmail.com>
2024-10-28 10:55:19 +0200

    Fix CSS build process for missing classes and

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-module.exports = {
   content: [
     "./*.{html,js}",
-    "./assets/**/*.{html,js}",
+    "./assets/**/*.{html,js,css}",
+    "./src/**/*.{html,js,css}",
     "./node_modules/flowbite/**/*.js",
+  ],
+  safelist: [
+    'badge',
+    'badge-success',
+    'badge-warning',
+    'badge-danger',
+    'datatable-wrapper',
+    'datatable-input',
+    'datatable-selector',
+    'datatable-pagination',
+    {
+      pattern: /^datatable-/,
+      variants: ['dark', 'hover', 'dark:hover']
+    }
   ],
   darkMode: "class",
   theme: {
